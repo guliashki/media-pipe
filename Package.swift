932a9ba71861826d9cb3f88f9227c5597747c373
Anka <runner@sat12-bq150-495d1ec5-1ce7-487b-99be-7c2757b72011-2238940E7FE8.local>
2025-11-07 01:48:25 +0000

    Auto-update package via GitHub Actions

--- Package.swift
+++ Package.swift
@@ -36,17 +36,17 @@ let package = Package(
         .binaryTarget(
             name: "MediaPipeTasksGenAI",
             url: "https://github.com/paescebu/SwiftTasksGenAI/releases/download/0.10.24/MediaPipeTasksGenAI.xcframework.zip",
-            checksum: "4a79aacb7864baf142448353a5131f48767bf861670cb25c70f682f97171e066"
+            checksum: "96f53666922282c5a332a6595c53716180d09d4fbca090469a25d487bc1288aa"
         ),
         .binaryTarget(
             name: "MediaPipeGenAICLibraries",
             url: "https://github.com/paescebu/SwiftTasksGenAI/releases/download/0.10.24/MediaPipeGenAICLibraries.xcframework.zip",
-            checksum: "681771c1c7234a910d03dabeb33fc7cdc3314731af2ef81f828bfc47b2a6cf46"
+            checksum: "1e7686c1552915d4e31321690f2aed0b1a11146d38d33ac5c86e8e1a8c2350df"
         ),
         .binaryTarget(
             name: "MediaPipeTasksGenAIC",
             url: "https://github.com/paescebu/SwiftTasksGenAI/releases/download/0.10.24/MediaPipeTasksGenAIC.xcframework.zip",
-            checksum: "298c27c204c14cd3a8b737493f11515b9229590aac676f844fba26063b2f3b38"
+            checksum: "ceff55c55f68e6de67b892ec15754ba966a25e5697328399a074092c6d6cbfe9"
         ),
         .executableTarget(
             name: "UpdatePackage",
